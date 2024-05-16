5caabae797c0e2c450846e3519b69e8081c7127f
ahmetttsevinc <ahmet.sevinc@bahcesehir.edu.tr>
2024-05-16 14:42:32 +0300

    complete SignUp page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,8 +8,8 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to={"/"}>
           <h1 className="font-bold text-sm sm:text-xl">
-            <span className="text-slate-500">Ahmet</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-green-500">Green</span>
+            <span className="text-slate-500">Place</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
